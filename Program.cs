3cac87eb72a48155bc857ba177594e6e62b4caa1
recenteagle777 <39369500+recenteagle777@users.noreply.github.com>
2018-05-17 14:06:57 +0400

    First upload
    Addition Program in C# using out

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+            Program n = new Program();
+            n.inputMethod(out x, out y);
+            Console.WriteLine("Addition of two integers are: {0}", x+y);
+            Console.ReadKey();
+        }
+
+        public void inputMethod(out int a, out int b)
+        {
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            
+        }
+    }
+}
